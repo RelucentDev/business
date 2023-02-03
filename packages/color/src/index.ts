f1203f3f2a09b3915eb4aa891009ef3982898aae
Dom Webber <dom.webber@hotmail.com>
2023-02-03 21:18:55 +0000

    inspired by GitHub's Primer Design system functional values

--- packages/color/src/index.ts
+++ packages/color/src/index.ts
@@ -65,20 +65,12 @@
  */
 const core = {
   /**
-   * White.
-   * Based on #ffffff.
-   *
-   * Use Cases:
-   *
-   * - Primary White #ffffff (white).
-   * - Neutral Light #ffffff (white).
-   *
+   * #ffffff
    */
   white: "hsl(0 0% 100% / <alpha-value>)",
 
   /**
-   * *"Black".*
-   * Equivalent to #020B17 (blue-10).
+   * #020B17
    *
    * @see core.blue[10];
    */
@@ -89,18 +81,49 @@ const core = {
    * Based on #D84231.
    */
   red: {
+    /**
+     * #330e0a
+     */
     10: "hsl(6 68% 12% / <alpha-value>)",
+
+    /**
+     * #5e1a12
+     */
     20: "hsl(6 68% 22% / <alpha-value>)",
+
+    /**
+     * #89251a
+     */
     30: "hsl(6 68% 32% / <alpha-value>)",
 
     /**
-     * Negative Red.
+     * #b43122
      */
     40: "hsl(6 68% 42% / <alpha-value>)",
+
+    /**
+     * #d84231
+     */
     50: "hsl(6 68% 52% / <alpha-value>)",
+
+    /**
+     * #e0695c
+     */
     60: "hsl(6 68% 62% / <alpha-value>)",
+
+    /**
+     * #e89187
+     */
     70: "hsl(6 68% 72% / <alpha-value>)",
+
+    /**
+     * #f0b8b2
+     */
     80: "hsl(6 68% 82% / <alpha-value>)",
+
+    /**
+     * #f8e0dd
+     */
     90: "hsl(6 68% 92% / <alpha-value>)",
   },
 
@@ -109,18 +132,49 @@ const core = {
    * Based on #FF7214.
    */
   orange: {
+    /**
+     * #471d00
+     */
     10: "hsl(24 100% 14% / <alpha-value>)",
+
+    /**
+     * #7a3100
+     */
     20: "hsl(24 100% 24% / <alpha-value>)",
+
+    /**
+     * #ad4500
+     */
     30: "hsl(24 100% 34% / <alpha-value>)",
 
     /**
-     * Severe Orange.
+     * #e05a00
      */
     40: "hsl(24 100% 44% / <alpha-value>)",
+
+    /**
+     * #ff7214
+     */
     50: "hsl(24 100% 54% / <alpha-value>)",
+
+    /**
+     * #ff9147
+     */
     60: "hsl(24 100% 64% / <alpha-value>)",
+
+    /**
+     * #ffaf7a
+     */
     70: "hsl(24 100% 74% / <alpha-value>)",
+
+    /**
+     * #ffcead
+     */
     80: "hsl(24 100% 84% / <alpha-value>)",
+
+    /**
+     * #ffede0
+     */
     90: "hsl(24 100% 94% / <alpha-value>)",
   },
 
@@ -129,18 +183,49 @@ const core = {
    * Based on #DFC416.
    */
   yellow: {
+    /**
+     * #252104
+     */
     10: "hsl(52 82% 8% / <alpha-value>)",
+
+    /**
+     * #544a08
+     */
     20: "hsl(52 82% 18% / <alpha-value>)",
+
+    /**
+     * #82720d
+     */
     30: "hsl(52 82% 28% / <alpha-value>)",
 
     /**
-     * Warning Yellow.
+     * #b09b11
      */
     40: "hsl(52 82% 38% / <alpha-value>)",
+
+    /**
+     * #dfc416
+     */
     50: "hsl(52 82% 48% / <alpha-value>)",
+
+    /**
+     * #ecd43c
+     */
     60: "hsl(52 82% 58% / <alpha-value>)",
+
+    /**
+     * #f0de6a
+     */
     70: "hsl(52 82% 68% / <alpha-value>)",
+
+    /**
+     * #f5e999
+     */
     80: "hsl(52 82% 78% / <alpha-value>)",
+
+    /**
+     * #f9f3c7
+     */
     90: "hsl(52 82% 88% / <alpha-value>)",
   },
 
@@ -149,18 +234,49 @@ const core = {
    * Based on #51B358.
    */
   green: {
+    /**
+     * #112713
+     */
     10: "hsl(124 39% 11% / <alpha-value>)",
+
+    /**
+     * #214a23
+     */
     20: "hsl(124 39% 21% / <alpha-value>)",
+
+    /**
+     * #306e34
+     */
     30: "hsl(124 39% 31% / <alpha-value>)",
 
     /**
-     * Positive Green.
+     * #409145.
      */
     40: "hsl(124 39% 41% / <alpha-value>)",
+
+    /**
+     * #51b358
+     */
     50: "hsl(124 39% 51% / <alpha-value>)",
+
+    /**
+     * #75c27a
+     */
     60: "hsl(124 39% 61% / <alpha-value>)",
+
+    /**
+     * #98d29c
+     */
     70: "hsl(124 39% 71% / <alpha-value>)",
+
+    /**
+     * #bce1be
+     */
     80: "hsl(124 39% 81% / <alpha-value>)",
+
+    /**
+     * #dff1e0
+     */
     90: "hsl(124 39% 91% / <alpha-value>)",
   },
 
@@ -170,24 +286,48 @@ const core = {
    */
   blue: {
     /**
-     * Black, Neutral Dark, Neutral Dark Muted (with opacity), Neutral Dark Subtle (with opacity).
+     * 020b17
      */
     10: "hsl(214 82% 5% / <alpha-value>)",
 
     /**
-     * Primary Blue.
+     * #072246
      */
     20: "hsl(214 82% 15% / <alpha-value>)",
+
+    /**
+     * #0b3974
+     */
     30: "hsl(214 82% 25% / <alpha-value>)",
+
+    /**
+     * #104fa2
+     */
     40: "hsl(214 82% 35% / <alpha-value>)",
+
+    /**
+     * #1566d1
+     */
     50: "hsl(214 82% 45% / <alpha-value>)",
 
     /**
-     * Secondary Blue, Action Blue.
+     * #2e80ea
      */
     60: "hsl(214 82% 55% / <alpha-value>)",
+
+    /**
+     * #5d9cef
+     */
     70: "hsl(214 82% 65% / <alpha-value>)",
+
+    /**
+     * #8bb8f4
+     */
     80: "hsl(214 82% 75% / <alpha-value>)",
+
+    /**
+     * #b9d5f8
+     */
     90: "hsl(214 82% 85% / <alpha-value>)",
   },
 
@@ -196,18 +336,49 @@ const core = {
    * Based on #690BE5.
    */
   violet: {
+    /**
+     * #100222
+     */
     10: "hsl(266 91% 7% / <alpha-value>)",
+
+    /**
+     * #260453
+     */
     20: "hsl(266 91% 17% / <alpha-value>)",
+
+    /**
+     * #3c0684
+     */
     30: "hsl(266 91% 27% / <alpha-value>)",
 
     /**
-     * Discovery Violet.
+     * #5308b4
      */
     40: "hsl(266 91% 37% / <alpha-value>)",
+
+    /**
+     * #690be5
+     */
     50: "hsl(266 91% 47% / <alpha-value>)",
+
+    /**
+     * #842ef5
+     */
     60: "hsl(266 91% 57% / <alpha-value>)",
+
+    /**
+     * #a15ef7
+     */
     70: "hsl(266 91% 67% / <alpha-value>)",
+
+    /**
+     * #bd8ffa
+     */
     80: "hsl(266 91% 77% / <alpha-value>)",
+
+    /**
+     * #dac0fc
+     */
     90: "hsl(266 91% 87% / <alpha-value>)",
   },
 
@@ -216,22 +387,49 @@ const core = {
    * Based on #726D97.
    */
   gray: {
+    /**
+     * #181721
+     */
     10: "hsl(247 17% 11% / <alpha-value>)",
+
+    /**
+     * #2f2c3f
+     */
     20: "hsl(247 17% 21% / <alpha-value>)",
+
+    /**
+     * #45425c
+     */
     30: "hsl(247 17% 31% / <alpha-value>)",
+
+    /**
+     * #5b577a
+     */
     40: "hsl(247 17% 41% / <alpha-value>)",
 
     /**
-     * Neutral, Neutral Muted (with opacity), Neutral Subtle (with opacity).
+     * #726d97
      */
     50: "hsl(247 17% 51% / <alpha-value>)",
+
+    /**
+     * #8f8bac
+     */
     60: "hsl(247 17% 61% / <alpha-value>)",
+
+    /**
+     * #aba8c2
+     */
     70: "hsl(247 17% 71% / <alpha-value>)",
 
     /**
-     * Neutral Light Muted (with opacity), Neutral Light Subtle (with opacity).
+     * #c8c6d7
      */
     80: "hsl(247 17% 81% / <alpha-value>)",
+
+    /**
+     * #e5e4ec
+     */
     90: "hsl(247 17% 91% / <alpha-value>)",
   },
 };
@@ -271,216 +469,191 @@ const core = {
  * @author Dom Webber <dom.webber@hotmail.com>
  */
 const semantic = {
-  primary: {
+  fg: {
     /**
-     * Primary Blue.
-     * ```semantic-primary-blue```
+     * Default foreground colour.
+     * This colour should be used for default text and icons in an interface.
+     * It should be used for body content, titles and labels.
      */
-    blue: core["blue"][20],
+    DEFAULT: core["blue"][10].replace("<alpha-value>", "1"),
 
     /**
-     * Primary White.
-     * ```semantic-primary-white```
+     * Muted foreground colour.
+     * This colour should be used for content that is secondary or that
+     * provides additional context but is not critical to understanding the
+     * flow of an interface.
      */
-    white: core["white"],
+    muted: core["blue"][10].replace("<alpha-value>", "0.85"),
 
     /**
-     * Primary Black.```
+     * Subtle foreground colour.
+     * This colour should be used for placeholder text, icons or decorative
+     * foregrounds.
      */
-    black: core["black"],
-  },
-  secondary: {
-    /**
-     * Secondary Blue.
-     * ```semantic-secondary-blue`
-     */
-    blue: core["blue"][40],
+    subtle: core["blue"][10].replace("<alpha-value>", "0.75"),
 
-    /**
-     * Secondary Red.
-     * ```semantic-secondary-red```
-     */
-    red: core["red"][30],
-  },
-  neutral: {
-    /**
-     * Neutral.
-     * ```semantic-neutral```
-     */
-    DEFAULT: core["gray"][50].replace("<alpha-value>", "1"),
-
-    /**
-     * Neutral Muted.
-     * ```semantic-neutral-muted```
-     */
-    muted: core["gray"][50].replace("<alpha-value>", "0.5"),
-
-    /**
-     * Neutral Subtle.
-     * ```semantic-neutral-subtle```
-     */
-    subtle: core["gray"][50].replace("<alpha-value>", "0.4"),
-    // Note: The above neutral colors currently have no decided meaning or use-case
-
-    light: {
-      /**
-       * Neutral Light.
-       * ```semantic-neutral-light```
-       */
-      DEFAULT: core["white"].replace("<alpha-value>", "1"),
-
-      /**
-       * Neutral Muted.
-       * ```semantic-neutral-light-muted```
-       */
-      muted: core["gray"][80].replace("<alpha-value>", "0.3"),
-
-      /**
-       * Neutral Subtle.
-       * ```semantic-neutral-light-subtle```
-       */
-      subtle: core["gray"][80].replace("<alpha-value>", "0.2"),
+    on: {
+      emphasis: {
+        /**
+         * On Emphasis foreground colour.
+         * This colour should be used for foreground elements on emphasis
+         * variants of background colours.
+         */
+        DEFAULT: core["white"].replace("<alpha-value>", "1"),
+      },
     },
 
-    dark: {
-      /**
-       * Neutral Dark.
-       * ```semantic-neutral-dark```
-       */
-      DEFAULT: core["black"].replace("<alpha-value>", "1"),
+    /**
+     * Positive foreground colour.
+     * This colour should be used to emphasise a positive or successful message.
+     */
+    positive: core["green"][40].replace("<alpha-value>", "1"),
 
-      /**
-       * Neutral Dark Muted.
-       * ```semantic-neutral-dark-muted```
-       */
-      muted: core["black"].replace("<alpha-value>", "0.85"),
+    /**
+     * Warning foreground colour.
+     * This colour should be used to highlight text or icons that require the
+     * user's attention - this doesn't guarantee that action needs to be taken.
+     */
+    warning: core["yellow"][40].replace("<alpha-value>", "1"),
 
-      /**
-       * Neutral Dark Subtle.
-       * ```semantic-neutral-dark-subtle```
-       */
-      subtle: core["black"].replace("<alpha-value>", "0.75"),
+    /**
+     * Severe foreground colour.
+     * This colour should be used to emphasise a level of severity between
+     * warning and negative.
+     */
+    severe: core["orange"][40].replace("<alpha-value>", "1"),
+
+    /**
+     * Negative foreground colour.
+     * This colour should be used to emphasise an error or blocking state
+     * wherein action is required.
+     */
+    negative: core["red"][40].replace("<alpha-value>", "1"),
+
+    /**
+     * Discovery foreground colour.
+     * This colour should be used for "discovery" foreground elements, such as
+     * those relating to "support" or "something new".
+     */
+    discovery: core["purple"][40].replace("<alpha-value>", "1"),
+
+    /**
+     * Action foreground colour.
+     * This colour should be used for interactive text or icons like links or
+     * buttons.
+     */
+    action: core["blue"][40].replace("<alpha-value>", "1"),
+  },
+
+  bg: {
+    /**
+     * Emphasis background colour.
+     * This colour should be used for elements such as tooltips that provide
+     * additional information. It provides the highest contrast against the
+     * default background.
+     */
+    emphasis: core["blue"][10].replace("<alpha-value>", "1"),
+
+    /**
+     * Default background colour.
+     * This colour should be used as the primary background colour for all
+     * visual pages.
+     */
+    DEFAULT: core["white"].replace("<alpha-value>", "1"),
+
+    /**
+     * Subtle background colour.
+     * This colour should be used to provide visual rest and contrast against
+     * the default background.
+     */
+    subtle: core["gray"][80].replace("<alpha-value>", "0.3"),
+
+    /**
+     * Inset background colour.
+     * This colour can be used instead of the default background to create a
+     * focal point within a page, such as conversations or activity feeds,
+     * wherein cards or additional surfaces within the page use a different
+     * and contrasting background colour.
+     */
+    inset: core["gray"][80].replace("<alpha-value>", "0.2"),
+
+    action: {
+      DEFAULT: core["blue"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["blue"][50].replace("<alpha-value>", "1"),
+    },
+
+    positive: {
+      DEFAULT: core["green"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["green"][50].replace("<alpha-value>", "1"),
+    },
+
+    warning: {
+      DEFAULT: core["yellow"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["yellow"][50].replace("<alpha-value>", "1"),
+    },
+
+    severe: {
+      DEFAULT: core["orange"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["orange"][50].replace("<alpha-value>", "1"),
+    },
+
+    negative: {
+      DEFAULT: core["red"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["red"][50].replace("<alpha-value>", "1"),
+    },
+
+    discovery: {
+      DEFAULT: core["violet"][40].replace("<alpha-value>", "0.2"),
+
+      emphasis: core["violet"][50].replace("<alpha-value>", "1"),
     },
   },
 
-  positive: {
-    /**
-     * Positive.
-     * ```semantic-positive```
-     */
-    DEFAULT: core["green"][40].replace("<alpha-value>", "1"),
+  border: {
+    DEFAULT: core["gray"][50].replace("<alpha-value>", "0.1"),
 
-    /**
-     * Positive Muted.
-     * ```semantic-positive-muted```
-     */
-    muted: core["green"][40].replace("<alpha-value>", "0.4"),
+    muted: core["gray"][70].replace("<alpha-value>", "0.1"),
 
-    /**
-     * Positive Subtle.
-     * ```semantic-positive-subtle```
-     */
-    subtle: core["green"][40].replace("<alpha-value>", "0.2"),
-  },
+    action: {
+      DEFAULT: core["blue"][40].replace("<alpha-value>", "0.4"),
 
-  warning: {
-    /**
-     * Warning.
-     * ```semantic-warning```
-     */
-    DEFAULT: core["yellow"][40].replace("<alpha-value>", "1"),
+      emphasis: core["blue"][40].replace("<alpha-value>", "1"),
+    },
 
-    /**
-     * Warning Muted.
-     * ```semantic-warning-muted```
-     */
-    muted: core["yellow"][40].replace("<alpha-value>", "0.4"),
+    positive: {
+      DEFAULT: core["green"][40].replace("<alpha-value>", "0.4"),
 
-    /**
-     * Warning Subtle.
-     * ```semantic-warning-subtle```
-     */
-    subtle: core["yellow"][40].replace("<alpha-value>", "0.2"),
-  },
+      emphasis: core["green"][40].replace("<alpha-value>", "1"),
+    },
 
-  severe: {
-    /**
-     * Severe.
-     * ```semantic-severe```
-     */
-    DEFAULT: core["orange"][40].replace("<alpha-value>", "1"),
+    warning: {
+      DEFAULT: core["yellow"][40].replace("<alpha-value>", "0.4"),
 
-    /**
-     * Severe Muted.
-     * ```semantic-severe-muted```
-     */
-    muted: core["orange"][40].replace("<alpha-value>", "0.4"),
+      emphasis: core["yellow"][40].replace("<alpha-value>", "1"),
+    },
 
-    /**
-     * Severe Subtle.
-     * ```semantic-severe-subtle```
-     */
-    subtle: core["orange"][40].replace("<alpha-value>", "0.2"),
-  },
+    severe: {
+      DEFAULT: core["orange"][40].replace("<alpha-value>", "0.4"),
 
-  negative: {
-    /**
-     * Negative.
-     * ```semantic-negative```
-     */
-    DEFAULT: core["red"][40].replace("<alpha-value>", "1"),
+      emphasis: core["orange"][40].replace("<alpha-value>", "1"),
+    },
 
-    /**
-     * Negative Muted.
-     * ```semantic-negative-muted```
-     */
-    muted: core["red"][40].replace("<alpha-value>", "0.4"),
+    negative: {
+      DEFAULT: core["red"][40].replace("<alpha-value>", "0.4"),
 
-    /**
-     * Negative Subtle.
-     * ```semantic-negative-subtle```
-     */
-    subtle: core["red"][40].replace("<alpha-value>", "0.2"),
-  },
+      emphasis: core["red"][40].replace("<alpha-value>", "1"),
+    },
 
-  discovery: {
-    /**
-     * Discovery.
-     * ```semantic-discovery```
-     */
-    DEFAULT: core["violet"][40].replace("<alpha-value>", "1"),
+    discovery: {
+      DEFAULT: core["purple"][40].replace("<alpha-value>", "0.4"),
 
-    /**
-     * Discovery Muted.
-     * ```semantic-discovery-muted```
-     */
-    muted: core["violet"][40].replace("<alpha-value>", "0.4"),
-
-    /**
-     * Discovery Subtle.
-     * ```semantic-discovery-subtle```
-     */
-    subtle: core["violet"][40].replace("<alpha-value>", "0.2"),
-  },
-
-  action: {
-    /**
-     * Action.
-     * ```semantic-action```
-     */
-    DEFAULT: core["blue"][50].replace("<alpha-value>", "1"),
-
-    /**
-     * Action Muted.
-     * ```semantic-action-muted```
-     */
-    muted: core["blue"][50].replace("<alpha-value>", "0.4"),
-
-    /**
-     * Action Subtle.
-     * ```semantic-action-subtle```
-     */
-    subtle: core["blue"][50].replace("<alpha-value>", "0.2"),
+      emphasis: core["purple"][40].replace("<alpha-value>", "1"),
+    },
   },
 };
 
